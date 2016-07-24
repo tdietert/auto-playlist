e098f3108ef0bda64b59b2009dbcd5896757a986
tdietert <tdietert@mail.usf.edu>
2016-07-23 21:04:48 -0400

    Removed unnecessary files

--- app/Main.hs
+++ app/Main.hs
@@ -9,11 +9,11 @@ import           System.Environment  (getArgs)
 
 main :: IO ()
 main = do
-  (clientId:g:genres) <- map T.pack <$> getArgs
+  (clientId:genres) <- map T.pack <$> getArgs
   manager <- newManager defaultManagerSettings
   let (SoundcloudClient trackClient) = makeAPIClient
       trackAPI = trackClient (Just clientId)
-  res <- runExceptT $ (searchTracksByGenre trackAPI) (g:genres) manager scApiBaseURL
+  res <- runExceptT $ (searchTracksByGenre trackAPI) genres manager scApiBaseURL
   case res of 
     Left err -> putStrLn $ "Error: " ++ show err
     Right tracks -> mapM_ print tracks
